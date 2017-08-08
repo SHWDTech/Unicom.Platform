2c7083f71387d0621f78bcf1ace131967491939d
御坂美琴 <autyan@outlook.com>
2017-08-08 22:40:02 +0800

    添加主页菜单

--- Unicom.Platform.Custom.RegisterWebSite/Controllers/HomeController.cs
+++ Unicom.Platform.Custom.RegisterWebSite/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using Unicom.Platform.Custom.RegisterWebSite.Entities;
+using Unicom.Platform.Custom.RegisterWebSite.Models.Bootstraptable;
 
 namespace Unicom.Platform.Custom.RegisterWebSite.Controllers
 {
@@ -11,6 +12,27 @@ namespace Unicom.Platform.Custom.RegisterWebSite.Controllers
             var projects = new UnicomDbContext().EmsProjects.ToList();
 
             return View(projects);
+        }
+
+        public ActionResult UnicomProjectTable(BootstraptablePost post)
+        {
+            var ctx = new UnicomDbContext();
+            var total = ctx.EmsProjects.Count();
+            var rows = ctx.EmsProjects.OrderBy(p => p.Code).Skip(post.offset).Take(post.limit)
+                .Select(prj => new
+                {
+                    prj.Code,
+                    prj.RegisterCode,
+                    prj.Name,
+                    prj.Telephone,
+                    prj.Superintendent
+                }).ToList();
+
+            return Json(new
+            {
+                total,
+                rows
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Unicom.Platform.Custom.RegisterWebSite/Models/Bootstraptable/BootstraptablePost.cs
@@ -0,0 +1,20 @@
+﻿
+// ReSharper disable InconsistentNaming
+
+namespace Unicom.Platform.Custom.RegisterWebSite.Models.Bootstraptable
+{
+    public class BootstraptablePost
+    {
+        public int offset { get; set; }
+
+        public int limit { get; set; }
+
+        public string sort { get; set; }
+
+        public string order { get; set; }
+
+        public string act { get; set; }
+
+        public string title { get; set; }
+    }
+}
